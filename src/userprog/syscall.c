774b573a3676b37cdafdc15e6079161221216e52
Ryan Ghoussainy <rg1523@oak02.doc.ic.ac.uk>
2024-10-30 16:01:40 +0000

    feat - System call handler now prints the system call number

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,8 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
+  printf ("system call number: %d\n", f->esp);
   thread_exit ();
 }
