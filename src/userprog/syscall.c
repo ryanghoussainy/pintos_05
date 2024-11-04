3d7e8b2b6d0ee1d12b1687bad551ebe7844a870a
samMadari <sam.shariatmadari-bafghi23@imperial.ac.uk>
2024-11-04 18:43:23 +0000

    feat[ss7923]: added write syscall to syscall.c and its header

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "filesys/file.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -30,4 +31,32 @@ void sys_exit (int status) {
 
   /* Terminates current thread. */
   thread_exit();
+}
+
+/* Writes bytes from buffer to the open file or console */
+int sys_write (int fd, const void *buffer, unsigned size) {
+  /* Writing to console and splitting buffer into defined chunks if needed */
+  int rem_size;
+  if (fd == 1){
+    int rem_size = size;
+    while (rem_size > 0) {
+      /* writes predefined amount to console or just
+         the remaining size if less */
+      if (rem_size < CONSOLE_INCR) {
+        putbuf(buffer, rem_size);
+        rem_size = 0;
+      } else {
+        putbuf(buffer, CONSOLE_INCR);
+        rem_size -= CONSOLE_INCR;
+      }
+    }
+    return size;
+  }
+
+  /* writing to a file only up until EOF, by calculating remaining space */
+  int file_size = file_length(fd);
+  off_t file_offset = file_tell(fd);
+  rem_size = file_size - file_offset;
+  file_write (fd, buffer, rem_size);
+  return rem_size;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,8 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#define CONSOLE_INCR 400
+
 void syscall_init (void);
 
 void sys_exit (int status);
+int sys_write (int fd, const void *buffer, unsigned size);
 
 #endif /* userprog/syscall.h */
