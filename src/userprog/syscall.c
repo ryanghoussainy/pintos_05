84d42f0b6b929817df5f820bf4ba2c3d656bda64
Alexander Chow <akc123@ic.ac.uk>
2024-11-05 00:50:27 +0000

    feat[akc123] - Added sys_create(), sys_remove(), sys_open() for syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,13 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "filesys/file.h"
+#include "devices/shutdown.h"
+#include "threads/synch.h"
+#include "filesys/filesys.h"
+#include <stdlib.h> 
+
+/* Lock used when handling files to ensure synchronisation. */
+static struct lock file_lock;
 
 static void syscall_handler (struct intr_frame *);
 
@@ -16,8 +23,14 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call number: %d\n", f->esp);
+  // printf ("system call number: %d\n", f->esp);
   thread_exit ();
+}
+
+/* Terminates PintOS. */
+void sys_halt (void) {
+  /* Calls shutdown_power_off() from devices/shutdown.h. */
+  shutdown_power_off();
 }
 
 /* Terminate this process. */
@@ -59,4 +72,89 @@ int sys_write (int fd, const void *buffer, unsigned size) {
   rem_size = file_size - file_offset;
   file_write (fd, buffer, rem_size);
   return rem_size;
+}
+
+/* Creates a new file with the given name and initial size. */
+bool sys_create (const char *file, unsigned initial_size) {
+  /* Checks if file is NULL. */
+  if (file == NULL) {
+    sys_exit(-1);
+  }
+
+  /* Acquires file lock to ensure synchronisation. */
+  lock_acquire(&file_lock);
+
+  /* Calls filesys_create() from filesys/filesys.c. */
+  bool success = filesys_create(file, initial_size);
+
+  /* Releases file lock. */
+  lock_release(&file_lock);
+
+  /* Returns if operation was successful or not. */
+  return success;
+}
+
+bool sys_remove (const char *file) {
+  /* Checks if file is NULL. */
+  if (file == NULL) 
+  {
+    sys_exit(-1);
+  }
+
+  /* Acquires file lock to ensure synchronisation. */
+  lock_acquire(&file_lock);
+
+  /* Calls filesys_remove() from filesys/filesys.c. */
+  bool success = filesys_remove(file);
+
+  /* Releases file lock. */
+  lock_release(&file_lock);
+
+  /* Returns if operation was successful or not. */
+  return success;
+}
+
+int sys_open (const char *file) {
+
+  /* Checks if file is NULL. */
+  if (file == NULL) 
+  {
+    return -1;
+  }
+
+  /* Acquires file lock to ensure synchronisation. */
+  lock_acquire(&file_lock);
+
+  /* Calls filesys_open() from filesys/filesys.c. */
+  struct file *f = filesys_open(file);
+
+  /* Checks if file is NULL. */
+  if (f == NULL) 
+  {
+    return -1;
+  }
+
+  /* Creates an opened file struct. */
+  struct o_file *cur_o_file = malloc(sizeof(struct o_file));
+
+  /* Allocate memory for struct o_file. */
+  if (cur_o_file == NULL) 
+  {
+    
+    file_close(f);
+    return -1;
+  }
+  /* Sets the file descriptor and file. */
+  struct thread *cur = thread_current();
+  cur_o_file->fd = cur->next_fd++;
+  cur_o_file->file = f;
+
+  /* Inserts the file descriptor into the hash table of running process. */
+  hash_insert(&cur->file_descriptors, &cur_o_file->fd_elem);
+
+  /* Releases file lock. */
+  lock_release(&file_lock);
+
+  /* Returns the file descriptor. */
+  return cur_o_file->fd;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,11 +1,28 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <lib/kernel/hash.h> 
+
 #define CONSOLE_INCR 400
+
+/*
+ * Contains a file opened by a process.
+ */
+struct o_file {
+    int fd;                     /* The file descriptor */
+    struct file *file;          /* Contents of loaded file */
+    struct hash_elem fd_elem;   /* To put open files in a hash table of the process */
+};
 
 void syscall_init (void);
 
+void sys_halt (void);
 void sys_exit (int status);
 int sys_write (int fd, const void *buffer, unsigned size);
+bool sys_create (const char *file, unsigned initial_size);
+bool sys_remove (const char *file);
+int sys_open (const char *file);
 
 #endif /* userprog/syscall.h */
