50c0a1e45d7915b2679d4770c038a3c360d63985
alexander mitchiner <=>
2024-11-08 15:52:18 +0000

    fix [am5523] freeing hash table in process_exit

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -139,6 +139,10 @@ process_exit (void)
   struct thread *cur = thread_current ();
   uint32_t *pd;
 
+  /* Free hash table and containing data */
+  hash_destroy(cur->file_descriptors, NULL);
+  free(cur->file_descriptors);
+
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
