751f79b921472065a315623bea2367a68cba800f
Ryan Ghoussainy <rg1523@pixel29.doc.ic.ac.uk>
2024-11-09 13:30:51 +0000

    fix - Only yield current thread after sema_up() if it has a lower priority than the waking thread.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -116,21 +116,26 @@ sema_up (struct semaphore *sema)
   enum intr_level old_level;
 
   ASSERT (sema != NULL);
+  struct thread *t = NULL;
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters))
   {
     list_sort(&sema->waiters, thread_more, NULL); 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+    t = list_entry (list_pop_front (&sema->waiters),
+                    struct thread, elem);
+    thread_unblock (t);
   }
   sema->value++;
   intr_set_level (old_level);
 
-  if (intr_context()) {
-    intr_yield_on_return();
-  } else {
-    thread_yield();
+  if (t != NULL && thread_get_effective_priority(t) > thread_get_priority())
+  {
+    if (intr_context()) {
+      intr_yield_on_return();
+    } else {
+      thread_yield();
+    }
   }
 }
 
